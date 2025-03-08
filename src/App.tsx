6f275631cd09729a6f56dafcecf4bd684975f7d0
ILYA Loginov <ILYA_13@bk.ru>
2025-03-08 16:23:51 +0300

    move components to components-folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@
 // import reactLogo from './assets/react.svg'
 
 import './App.css'
-import Video from './Video'
+import Video from './Video/Video'
 import { VIDEOS } from './videos'
 
 function App() {
    
   return (
       <>
-     <div className='video-container'>
-        
-       {VIDEOS.map((video)=>(
-        <Video title={video.title} chanelName={video.chanelName}  img={video.img} />
+     <div className='video-container'>        
+       {VIDEOS.map((video)=>(        
+        <Video key = {video.id} title={video.title} chanelName={video.chanelName}  img={video.img} />
        ))
 
        }
