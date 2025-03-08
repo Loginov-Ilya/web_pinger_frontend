2a7685f10a18e8937cd3430bc3aa0fa383a2c53b
ILYA Loginov <ILYA_13@bk.ru>
2025-03-08 15:31:32 +0300

    create test project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import reactLogo from './assets/react.svg'
+import './App.css'
+function App() {
+   const result ="my first react app"
+  return (
+      <>
+      <h1>Vite + React {result} <img src={reactLogo} alt="react logo" /></h1>
+      <p>asdasdad</p>
+      </>
+   
+  )
+}
+
+export default App
